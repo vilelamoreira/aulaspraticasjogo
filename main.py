a615991ec581c7777d2bd2719e853334cc0bffda
felipe@felipe.com <felipevilelamoreira@email.com>
2026-05-15 19:54:28 -0300

    Primera alteracao

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+window = pygame.display.set_mode(size = (600, 480))
+print('Setup End')
+
+print('Setup Start')
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() #End Pygame
+
+
