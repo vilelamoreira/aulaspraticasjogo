48b6547af4a13ebf3966b48fe9d4e3dab88b73c7
vilelamoreira <felipevilelamoreira@gmail.com>
2026-05-15 20:13:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ while True:
     # Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('Quiting...')
             pygame.quit() # Close Window
             quit() #End Pygame
 
